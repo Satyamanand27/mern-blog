8ce6cd233c6b523eb670fe898ef0370070c18a86
Satyam Anand <satyamanand2012@gmail.com>
2025-04-20 13:49:14 +0530

    Create SignUp API route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 // dotenv.config();
-dotenv.config({ path: '../.env' });
+// dotenv.config({ path: '../.env' });
+// console.log('MONGO URI:', process.env.MONGO);
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// These two lines give us __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from the parent directory
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+
 
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -22,3 +35,4 @@ app.listen(2000,()=>{
 )
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
