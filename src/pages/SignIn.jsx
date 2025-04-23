63b592175680661892b5445e8186375d5ac3ab32
Satyam Anand <satyamanand2012@gmail.com>
2025-04-23 12:27:21 +0530

    Add redux persist

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -42,6 +42,9 @@ export default function SignIn() {
       dispatch(signInFailure(error.message));
     }
   };
+  
+  
+  
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
